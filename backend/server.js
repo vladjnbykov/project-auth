e379d68d5af45234bd40fd92242e93f1b6670b5e
vladjnbykov <67795638+vladjnbykov@users.noreply.github.com>
2021-05-25 11:46:22 +0200

    messages, user signup, token issue resolved

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,88 @@
+/* eslint-disable linebreak-style */
 import express from 'express'
-import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
+import crypto from 'crypto'
+import bcrypt from 'bcrypt'
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/authAPI"
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
 mongoose.Promise = Promise
 
 // Defines the port the app will run on. Defaults to 8080, but can be 
 // overridden when starting the server. For example:
 //
 //   PORT=9000 npm start
+
+const Note = mongoose.model('Note', {
+  message: String
+})
+
+const User = mongoose.model('User', {
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  accessToken: {
+    type: String,
+    default: () => crypto.randomBytes(128).toString('hex')
+  }
+})
+
 const port = process.env.PORT || 8080
 const app = express()
 
 // Add middlewares to enable cors and json body parsing
 app.use(cors())
-app.use(bodyParser.json())
+app.use(express.json())
 
 // Start defining your routes here
 app.get('/', (req, res) => {
   res.send('Hello world')
+})
+
+app.get('/notes', async (req, res) => {
+  const notes = await Note.find()
+  res.json(notes)
+
+})
+
+app.post('/notes', async (req, res) => {
+  const { message } = req.body
+
+  try {
+    const newNote = await new Note({ message }).save()
+
+    res.json(newNote)
+  } catch (error) {
+    res.status(400).json({ message: "Invalid request", error })
+  }
+})
+
+app.post('/signup', async (req, res) => {
+  const { username, password } = req.body
+
+  try {
+    const salt = bcrypt.genSaltSync()
+
+    const newUser = await new User({
+      username,
+      password: bcrypt.hashSync(password, salt)
+    }).save()
+
+    res.json({
+      userID: newUser._id,
+      username: newUser.username,
+      accessToken: newUser.accessToken
+    });
+  } catch (error) {
+    res.status(400).json({ message: 'Invalid request', error })
+  }
 })
 
 // Start the server
