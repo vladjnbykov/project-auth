50d7341d651f94a003614e456e28b1480a484f74
vladjnbykov <67795638+vladjnbykov@users.noreply.github.com>
2021-05-28 21:21:01 +0200

    setting env

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 /* eslint-disable linebreak-style */
+import dotenv from 'dotenv'
 import express from 'express'
 import cors from 'cors'
 import mongoose from 'mongoose'
 import crypto from 'crypto'
 import bcrypt from 'bcrypt'
+
+dotenv.config()
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/authAPI"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
